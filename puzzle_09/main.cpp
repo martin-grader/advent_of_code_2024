df34af49981db93d426c9b669642fb1feb3e06f6
Martin Grader <martingrader@gmail.com>
2024-12-17 09:14:33 +0100

    add small comment

--- puzzle_09/main.cpp
+++ puzzle_09/main.cpp
@@ -8,5 +8,6 @@ int main() {
     const std::string puzzle = get_puzzle();
     const long int checksum = get_checksum(puzzle);
     std::cout << "checksum = " << checksum << std::endl;
+    // 6323903789307 is wrong
     return 0;
 }
